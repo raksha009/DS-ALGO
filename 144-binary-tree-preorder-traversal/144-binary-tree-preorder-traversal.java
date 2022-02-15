75a09c7e9c564b1d510ce12c2fdf2ef42983dd67
Raksha <57195964+raksha009@users.noreply.github.com>
2022-02-16 01:16:22 +0530

    Time: 0 ms (100.00%), Space: 42.7 MB (6.51%) - LeetHub

--- 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.java
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.java
@@ -13,25 +13,53 @@
  *     }
  * }
  */
+
+// Way1: Recursive with internal stack SC: O(n)  TC:O(n)
+// Way2: Iterative with external stack SC: O(n)  TC:O(n)
+// Way3: Iterative SC: O(1)  TC:O(n)  === Morris Traversal (Here)
 class Solution {
     
     List<Integer> arr;
     public List<Integer> preorderTraversal(TreeNode root) {
-        
         arr = new ArrayList<>();
         
-        // Way1: Recursive with internal stack SC: O(n)  TC:O(n)
-        preorder(root);
+        preorderMorris(root);
         return arr;   
     }
     
-    public void preorder(TreeNode root){
-        if (root == null) return;
+    public void preorderMorris(TreeNode root){
         
-        // Root processed
-        arr.add(root.val);
-        preorder(root.left);
-        preorder(root.right);
+        TreeNode curr = root;
+        while (curr != null){
+            if (curr.left != null){
+                TreeNode currp1 = curr.left;
+                while (currp1.right != null && currp1.right != curr){
+                    currp1 = currp1.right;
+                }
+                
+                // 1st time visit => left nodes are yet to be processed
+                // Form connection with curr and curr move left
+                // Process left node n move ahead
+                if (currp1.right == null) {
+                    arr.add(curr.val);
+                    currp1.right = curr;
+                    curr = curr.left;
+                }
+                
+                // 2nd time visit => all left nodes processed
+                // Break connection and curr move right
+                if (currp1.right == curr){
+                    currp1.right = null;
+                    curr = curr.right;
+                }
+                
+            }else{
+                // reached last level 
+                arr.add(curr.val);  // Processed current node
+                curr = curr.right;
+                
+            }
+        }
         
     }
 }
