79b800139587462c974c2d77265736da14b6744e
Raksha <57195964+raksha009@users.noreply.github.com>
2022-02-16 01:51:42 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (17.38%) - LeetHub

--- 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.java
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.java
@@ -14,9 +14,9 @@
  * }
  */
 
-// Way1: Recursive with internal stack SC: O(n)  TC:O(n)  (HERE)
+// Way1: Recursive with internal stack SC: O(n)  TC:O(n)  
 // Way2: Iterative with external stack SC: O(n)  TC:O(n)
-// Way3: Iterative SC: O(1)  TC:O(n)  === Morris Traversal 
+// Way3: Iterative SC: O(1)  TC:O(n)  === Morris Traversal (HERE) 
 
 class Solution {
     
@@ -25,16 +25,51 @@ class Solution {
         
         arr = new ArrayList<>();
         
-        postorder(root);
-        return arr;   
+        // Morris Traversal only had Inorder and PreOrder
+        // Using (Reverse PreOrder) ka reverse to find PostOrder
+        
+        reversePreOrderMorris(root);
+        
+        // Reversing (ReversePreOrder) to get Postorder
+        Collections.reverse(arr);   
+        return arr;
     }
     
-    public void postorder(TreeNode root){
+    // Changing .right to .left and vice versa (of Preorder)
+    public void reversePreOrderMorris(TreeNode root){
         
         if (root == null) return;
         
-        postorder(root.left);
-        postorder(root.right);
-        arr.add(root.val);
+        TreeNode curr = root;
+        while (curr != null){
+            if (curr.right != null){
+                TreeNode currp1 = curr.right;
+                while (currp1.left != null && currp1.left != curr){
+                    currp1 = currp1.left;
+                }
+                
+                // 1st time visit => left nodes are yet to be processed
+                // Form connection with curr and curr move left
+                // Process left node n move ahead
+                if (currp1.left == null) {
+                    arr.add(curr.val);
+                    currp1.left = curr;
+                    curr = curr.right;
+                }
+                
+                // 2nd time visit => all left nodes processed
+                // Break connection and curr move right
+                if (currp1.left == curr){
+                    currp1.left = null;
+                    curr = curr.left;
+                }
+                
+            }else{
+                // reached last level 
+                arr.add(curr.val);  // Processed current node
+                curr = curr.left;
+                
+            }
+        }
     }
 }
