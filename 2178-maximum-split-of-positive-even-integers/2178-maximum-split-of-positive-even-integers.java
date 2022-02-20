60b643efec7a1b8d73686dac13768ab7d84d63a8
Raksha <57195964+raksha009@users.noreply.github.com>
2022-02-20 12:44:36 +0530

    Time: 27 ms (70.00%), Space: 133.2 MB (20.00%) - LeetHub

--- 2178-maximum-split-of-positive-even-integers/2178-maximum-split-of-positive-even-integers.java
+++ 2178-maximum-split-of-positive-even-integers/2178-maximum-split-of-positive-even-integers.java
@@ -14,20 +14,33 @@ class Solution {
         
         long rem = finalSum;
         
-        HashSet<Long> set = new HashSet<>();
-        for (int i = 2; i < finalSum; i+=2){
-            if (rem <= 0) break;
-            long toadd = i;
+//         HashSet<Long> set = new HashSet<>();
+//         for (int i = 2; i < finalSum; i+=2){
+//             if (rem <= 0) break;
+//             long toadd = i;
             
             
-            if (rem <= 0 || set.contains(rem-toadd) || toadd == (rem-toadd)) break;
-            rem = rem-toadd;
-            ans.add(toadd);
-            set.add(toadd);
+//             if (rem <= 0 || set.contains(rem-toadd) || toadd == (rem-toadd)) break;
+//             rem = rem-toadd;
+//             ans.add(toadd);
+//             set.add(toadd);
             
+//         }
+        // if (rem > 0) ans.add(rem);
+        
+        // --------    OR   ----------
+        
+        long i = 2;
+        long curSum =  0;
+        while (curSum + i <= finalSum){
+            ans.add(i);
+            curSum += i;
+            i+=2;
         }
         
-        if (rem > 0) ans.add(rem);
+        // add remaining difference to the last value in answer list
+        ans.set(ans.size()-1, ans.get(ans.size()-1)+ finalSum - curSum);
+        
         return ans;
         
     }
